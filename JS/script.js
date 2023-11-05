9e125a2c0e0b19b791d5a6f45c679cf59e1501cb
evrlong <erilok51657@stud.noroff.no>
2023-11-05 19:53:22 +0100

    kk

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,6 @@
 function show_hide() {
-    document.getElementById('sidemenu').classList.toggle('active');
-}
 
-// Add an event listener to hide the sidemenu when clicking outside
-document.addEventListener('click', function (event) {
-    const sidemenu = document.getElementById('sidemenu');
-    if (!sidemenu.contains(event.target)) {
-        sidemenu.classList.remove('active');
+    document.getElementById('sidemenu').classList.toggle('active');
     }
-});
+    
+    
