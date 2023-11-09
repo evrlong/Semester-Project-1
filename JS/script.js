9a2b4d06d5834d0b9d1e94b79a0a3c870668b62a
evrlong <erilok51657@stud.noroff.no>
2023-11-09 17:33:42 +0100

    div

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,15 @@
-function show_hide() {
 
+function show_hide() {
     document.getElementById('sidemenu').classList.toggle('active');
+}
+
+
+document.getElementById("search-icon").addEventListener("click", function() {
+    document.getElementById("search-overlay").style.display = "block";
+});
+
+document.getElementById("search-overlay").addEventListener("click", function(e) {
+    if (e.target.id === "search-overlay") {
+        document.getElementById("search-overlay").style.display = "none";
     }
-    
-    
+});
